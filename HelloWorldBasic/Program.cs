7217d9458deb7f144a36e7eab2e61c6860e9e2d4
TasniaKheya <23kheya@gmail.com>
2024-06-04 15:53:46 +0600

    This is a test Project

---
+++ HelloWorldBasic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class firstProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
